f9ff9b9a3ba408b41a8c0d7104354993260cfee8
Joshua Sharp <josh.sharp@medipass.io>
2022-12-19 20:36:30 +1100

    feat: upgrade to typescript

---
+++ src/index.test.ts
@@ -0,0 +1,21 @@
+import { validateProviderNumber } from ".";
+
+test("returns true for a valid provider number", () => {
+  expect(validateProviderNumber("2429581T")).toBeTruthy();
+});
+
+test("returns false for an invalid provider number (more than 8 characters)", () => {
+  expect(validateProviderNumber("02429581T")).toBeFalsy();
+});
+
+test("returns false for an invalid provider number (incorrect check digit)", () => {
+  expect(validateProviderNumber("2429582T")).toBeFalsy();
+});
+
+test("returns false for an invalid provider number (no check digit)", () => {
+  expect(validateProviderNumber("2429582")).toBeFalsy();
+});
+
+test("returns false for an invalid provider number (5 digit provider-stem)", () => {
+  expect(validateProviderNumber("429581T")).toBeFalsy();
+});
