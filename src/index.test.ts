2c7fbe2a27c5b825b3594bdf784aee1466e4bbfb
Claudia <40446421+claudianadalin@users.noreply.github.com>
2026-07-17 13:43:59 +1000

    feat: add validateMedibankProviderNumber (#7)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { validateProviderNumber } from ".";
+import { validateMedibankProviderNumber, validateProviderNumber } from ".";
 
 test("returns true for a valid provider number", () => {
   expect(validateProviderNumber("2429581T")).toBeTruthy();
@@ -23,3 +23,23 @@ test("returns false for an invalid provider number (no check digit)", () => {
 test("returns false for an invalid provider number (5 digit provider-stem)", () => {
   expect(validateProviderNumber("429581T")).toBeFalsy();
 });
+
+test.each(["A200853K", "A204983L", "a204983l"])(
+  "returns true for a valid Medibank provider number (%s)",
+  (providerNumber) => {
+    expect(validateMedibankProviderNumber(providerNumber)).toBeTruthy();
+  }
+);
+
+test.each([
+  ["incorrect check digit", "A204983B"],
+  ["missing profession prefix", "204983L"],
+  ["invalid profession prefix", "!204983L"],
+  ["more than one profession prefix", "AA204983L"],
+  ["invalid length", "A2429581T"],
+])(
+  "returns false for an invalid Medibank provider number (%s)",
+  (_, providerNumber) => {
+    expect(validateMedibankProviderNumber(providerNumber)).toBeFalsy();
+  }
+);
